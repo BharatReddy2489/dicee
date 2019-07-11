4d27238b62cfffb0edde9de4cf270dee5e1a11cc
BharatReddy2489 <49775479+BharatReddy2489@users.noreply.github.com>
2019-07-11 20:56:53 +0530

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,14 @@
+
+  var x=Math.floor(Math.random()*6)+1;
+  var y=Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img1").setAttribute("src","images/dice"+x+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+y+".png");
+
+if (x>y){
+document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+} else if (x<y){
+document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+} else if (x===y){
+document.querySelector("h1").innerHTML="Draw!";
+}
